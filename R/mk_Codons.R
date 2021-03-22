99396df39651bfcee77f44bd7586c811d3548431
ThanosPAS <thanospas@yahoo.com>
2021-03-22 10:22:47 +0100

    Documentation re-submission

--- R/mk_Codons.R
+++ R/mk_Codons.R
@@ -1,5 +1,16 @@
 # R/mk_codons.R
 
+#` Making a Codonframe
+
+#` Takes a DNA Sequence and returns a list of triplets (Codons)
+
+#` @param DNA sequence
+#` @param s start of making codons in sequence
+
+#` return codons 
+
+#` @export
+
 
 mk_codons <- function(dna, s = 1){
   l = nchar(dna)
