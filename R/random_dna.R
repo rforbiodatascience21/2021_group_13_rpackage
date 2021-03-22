a8fd3150cc440e3a57abd7d20aa414d27863b97a
SofusHW <sofus.wii@gmail.com>
2021-03-22 09:57:30 +0000

    Adding documentation to function

--- R/random_dna.R
+++ R/random_dna.R
@@ -1,4 +1,14 @@
 # R/random_dna.R
+
+#` Takes a length and returns a DNA string of equal length
+
+#` @param length of wanted/expected DNA string
+
+#` return DNA 
+
+#` @export
+
+
 random_dna <- function(l){
   nucleotides <- sample(c("A", "T", "G", "C"), size = l, replace = TRUE)
   dna = paste0(nucleotides, collapse = "")
